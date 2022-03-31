7fda22e37b0401321933671f54e9049bc7035704
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-03-31 13:05:16 +0200

    created project

---
+++ src/QueryExecutor.Core/Common/IDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryExecutor.Core.Common
+{
+    public interface IDataContext
+    {
+
+    }
+}
